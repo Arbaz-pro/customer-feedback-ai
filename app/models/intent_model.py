84d0332ad7135b1f463f3bdc3eb9ab031bcc359c
Arbaz-pro <arbazpawaskar2@gmail.com>
2026-04-06 00:30:02 +0530

    deploy test8

--- app/models/intent_model.py
+++ app/models/intent_model.py
@@ -19,10 +19,10 @@
 #     return classifier
 import requests
 import os
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 
 # Load env (only works locally, safe for Render too)
-load_dotenv()
+# load_dotenv()
 
 HF_API_URL = "https://api-inference.huggingface.co/models/Arbaz04/aspect_model"
 HF_TOKEN = os.getenv("HF_TOKEN")  # store safely in env
