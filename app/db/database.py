70e29ffd5610fdcd3b06d7433c665d8fd38eccfe
Arbaz-pro <arbazpawaskar2@gmail.com>
2026-04-05 19:24:31 +0530

    render deployment testing

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,14 @@ from sqlalchemy import create_engine, text
 
 DATABASE_URL = "postgresql://neondb_owner:npg_Ur5woqD3FHsg@ep-spring-darkness-a1wuymli-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
-engine = create_engine(DATABASE_URL,pool_pre_ping=True,
-    connect_args={"connect_timeout": 5})
+engine = create_engine(DATABASE_URL,pool_pre_ping=True,pool_pre_ping=True,     
+    pool_recycle=180,      
+    pool_size=5,            
+    max_overflow=10,        
+    connect_args={
+        "connect_timeout": 10,
+        "sslmode": "require"
+    })
 
 def create_table():
     with engine.begin() as conn:
